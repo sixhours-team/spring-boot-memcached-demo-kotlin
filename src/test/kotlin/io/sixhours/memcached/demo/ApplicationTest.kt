ee6fbd4661a97a7ed7b3995622ba5a9ef34f9fee
Igor Bolic <igor.bolic@sixhours.io>
2018-01-04 12:19:05 +0100

    Update ApplicationTest.kt

--- src/test/kotlin/io/sixhours/memcached/demo/ApplicationTest.kt
+++ src/test/kotlin/io/sixhours/memcached/demo/ApplicationTest.kt
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.cache.CacheManager
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.cache.support.NoOpCacheManager
 import org.springframework.test.context.junit4.SpringRunner
 
